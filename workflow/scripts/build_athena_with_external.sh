423ce2314be5ca9f4e695e7303296d5218eb32f2
Xiangyang Ju <xju@lbl.gov>
2025-04-25 14:59:53 -0700

    add date

--- workflow/scripts/build_athena_with_external.sh
+++ workflow/scripts/build_athena_with_external.sh
@@ -12,7 +12,7 @@ usage() {
     exit 1
 }
 
-while getopts "i:o:m:t:" opt; do
+while getopts "i:o:t:" opt; do
     case $opt in
         i) INPUT_FILE="$OPTARG" ;;
         o) OUTPUT_FILE="$OPTARG" ;;
@@ -24,7 +24,7 @@ done
 OUTPUT_FILE=$(realpath "$OUTPUT_FILE")
 
 # Log the parameters
-echo "Running Athena build on top of atlasexternal."
+echo "Running Athena build on top of atlasexternal. $(date)"
 echo "Input File: $INPUT_FILE"
 echo "Output File: $OUTPUT_FILE"
 echo "Number of Threads: $WORKERS"
@@ -32,12 +32,14 @@ echo "Number of Threads: $WORKERS"
 # deactivate the herited python environment.
 source workflow/scripts/deactivate_python_env.sh
 
-# read the input file to get SOURCE_DIR, ASETUP.
-export G4PATH=/cvmfs/atlas-nightlies.cern.ch/repo/sw/main_Athena_x86_64-el9-gcc13-opt/Geant4
-export ATLASAuthXML=/global/cfs/cdirs/atlas/xju/data/xml
 
 source /global/cfs/cdirs/atlas/scripts/setupATLAS.sh
 setupATLAS
+
+# read the input file to get SOURCE_DIR, ASETUP.
+export G4PATH=/cvmfs/atlas-nightlies.cern.ch/repo/sw/main_Athena_x86_64-el9-gcc13-opt/Geant4
+export ATLASAuthXML=/global/cfs/cdirs/atlas/xju/data/xml
+export PATH=/cvmfs/sft.cern.ch/lcg/contrib/ninja/1.11.1/Linux-x86_64/bin:$PATH
 
 while read -r cmd; do
     echo "Running validation command: $cmd"
@@ -48,8 +50,8 @@ echo "Building Athena at $(pwd)"
 which python
 which gcc
 
-time ./athena/Projects/Athena/build.sh -acmi \
-  -x "-DATLAS_ENABLE_CI_TESTS=TRUE -DATLAS_EXTERNAL=${ATLASAuthXML} -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE" \
+time ./athena/Projects/Athena/build.sh -acmi -t Release \
+  -x "-DATLAS_ENABLE_CI_TESTS=TRUE -DATLAS_EXTERNAL=${ATLASAuthXML} -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE" \
   -k "-j ${WORKERS}" 2>&1 || { echo "Error: Athena build failed."; exit 1; }
 
 echo "SOURCE_DIR=${SOURCE_DIR}" > "$OUTPUT_FILE"
